36b4efba0b1fccc1c1ca3100088412190e771369
Juanma Santoyo <juanma@juanmasantyo.es>
2012-06-13 17:56:05 +0200

    Añadido algo de lógica básica del plugin

--- js/dev/README.txt
+++ js/dev/README.txt
@@ -5,8 +5,8 @@ This folder will include the javascript for the plugin, in a developement versio
 The file name will be the name of the plugin and the version. The version is based in 3 numbers:
 
 a: The version name. Starts as 1, increments with big functionalities.
-b: The subversion name. Starts as 1, increments with small functionalities.
-c: The revision name. Starts as 1, increments with bug fixes.
+b: The subversion name. Starts as 0, increments with small functionalities.
+c: The revision name. Starts as 0, increments with bug fixes.
 
 The directory base structure is:
 
---
+++ js/dev/jquery.jsPlugin-1.0.0.js
@@ -0,0 +1,53 @@
+﻿/*
+    jsPlugin by Juanma Santoyo, version 1.0.0.
+    http://www.juanmasantoyo.es
+*/
+
+(function ($)
+{
+    var globals = {
+        settings: null
+    };
+
+    var methods = {
+        // This method inits the jsCarousel
+        init: function (options)
+        {
+            var _this = this;
+
+            // Extends settings with passed options
+            globals.settings = $.extend({
+            
+            }, options);
+
+            return this.each(function ()
+            { 
+            
+            });
+        }
+        , _isPublic: function (method)
+        {
+            return method.charAt(0) != '_';
+        }
+    };
+
+    var callbacks = {
+    };
+
+    $.fn.jsPlugin = function (method)
+    {
+        // Call the method if exists.
+        if (methods[method] && methods._isPublic(method))
+        {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        }
+        else if (typeof method === 'object' || !method)
+        {
+            return methods.init.apply(this, arguments);
+        }
+        else
+        {
+            $.error('Method ' + method + ' does not exist on jQuery.jsPlugin');
+        }
+    }
+})(jQuery);
